63dfd7c12c13fb557121e098fa35971c72a743ff
satryawiguna <satrya@freshcms.net>
2022-08-15 15:57:40 +0800

    IN-004 Minor fix to add reference

--- pages/product/[id]/edit.js
+++ pages/product/[id]/edit.js
@@ -1,3 +1,5 @@
+import AdminHook from "../../../components/layouts/admin.hook";
+import ProductForm from "../../../components/ProductForm";
 import { useDispatch, useSelector } from "react-redux";
 import { productSelectors } from "../../../redux/features/productSlice";
 
